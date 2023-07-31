bf7e72ed2ca61373ddc5e2a0b6ea8bc6b98cc730
ANKITKUMAR1809 <skwedsak143@gmail.com>
2023-07-31 22:18:08 +0530

    making the main showable in resume with page

--- index.js
+++ index.js
@@ -4,12 +4,28 @@ function showLeft(){
     const elem= document.getElementById("left");
     elem.style.transition="3s";
     elem.style.width="100%";
-};
+}
 
 function showRight(){
     const elem= document.getElementById("right");
     const elem1= document.getElementById("rpro");
-    elem.style.transition="3s";
+    const elem2= document.getElementById("leftRight");
+    elem.style.transition="2s";
     elem1.style.display="block"
     elem.style.width="100%";
-};
+    elem2.style.width="50%";
+    elem2.style.width="50%";
+    elem2.style.display="block";
+}
+
+function showMain(){
+    const elem= document.getElementById("right");
+    const elem1= document.getElementById("rpro");
+    const elem2= document.getElementById("leftRight");
+    elem.style.transition="2s";
+    elem1.style.display="none"
+    elem.style.width="0px";
+    elem2.style.width="0px";
+    elem2.style.width="0px";
+    elem2.style.display="none";
+}
