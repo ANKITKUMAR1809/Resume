2a7212ac15da4cc39469a2f8911bb68993edca51
ANKITKUMAR1809 <skwedsak143@gmail.com>
2023-07-31 15:18:40 +0530

    added java script with animation

---
+++ index.js
@@ -0,0 +1,13 @@
+console.log("java is working");
+
+function showLeft(){
+    const elem= document.getElementById("left");
+    elem.style.transition="3s";
+    elem.style.width="100%";
+};
+
+function showRight(){
+    const elem= document.getElementById("right");
+    elem.style.transition="3s";
+    elem.style.width="100%";
+};
