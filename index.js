17f6398e945073d3f0396925ee342cce4201becf
ANKITKUMAR1809 <skwedsak143@gmail.com>
2023-08-01 17:56:40 +0530

    adding project detail and its repository link

--- index.js
+++ index.js
@@ -10,22 +10,27 @@ function showRight(){
     const elem= document.getElementById("right");
     const elem1= document.getElementById("rpro");
     const elem2= document.getElementById("leftRight");
+    const upright= document.getElementById("upright");
     elem.style.transition="2s";
     elem1.style.display="block"
     elem.style.width="100%";
     elem2.style.width="50%";
     elem2.style.width="50%";
-    elem2.style.display="block";
+    elem2.style.display="inline-block";
+    upright.style.display="inline-block";
+
 }
 
 function showMain(){
     const elem= document.getElementById("right");
     const elem1= document.getElementById("rpro");
     const elem2= document.getElementById("leftRight");
+    const upright= document.getElementById("upright");
     elem.style.transition="2s";
     elem1.style.display="none"
     elem.style.width="0px";
     elem2.style.width="0px";
     elem2.style.width="0px";
     elem2.style.display="none";
+    upright.style.display="none";
 }
